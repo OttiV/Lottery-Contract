dcf073c52ca6c35324e473b2fe793c6aed8d38d0
Vgnttv <o.vignani@gmail.com>
2022-08-07 12:07:44 +0200

    Update solidity version and adjust project

--- deploy.js
+++ deploy.js
@@ -1,9 +1,11 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
+
 const MNEMONIC = 'special join maple library another replace wrap rare shy wait scatter absurd'
 const INFURA_URL = 'https://rinkeby.infura.io/v3/2fa376e4b0644b269a73401ee4652e23'
 const provider = new HDWalletProvider(MNEMONIC, INFURA_URL);
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -11,10 +13,11 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(JSON.stringify(abi));
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
